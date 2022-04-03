954178b13142623df4d8dd5dbd90d1a1d8bbf91c
Guilherme Blanck <guilhermeblanck@gmail.com>
2022-04-03 18:03:53 -0300

    feat: user authenticaton on OpnBnk data request

--- app/controllers/v1/openbanking_controller.rb
+++ app/controllers/v1/openbanking_controller.rb
@@ -1,6 +1,22 @@
 class V1::OpenbankingController < ApplicationController
+  before_action :validate_user_token
   def qualification
     data = JSON.parse PlaceholderInfo.find_by_name('qualification').data
-    render json: data[rand data.size]
+    render json: {data: data[rand data.size]}
+  end
+
+  def identification
+    data = JSON.parse PlaceholderInfo.find_by_name('identification').data
+    render json: {data: data[rand data.size]}
+  end
+  
+  private
+
+  def validate_user_token
+    @u = User.find(params[:user][:id]) unless params[:user].nil? || params[:user][:id].nil?
+    if @u.nil? || params[:user][:token].nil? || @u.authentication_token != params[:user][:token]
+      head(:unauthorized)
+    end
+    
   end
 end
